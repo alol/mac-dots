e65df9b757d069c9b244868a2b40b40badbed13e
Ed James <ed.james.email@gmail.com>
2013-02-01 16:55:56 +0000

    git aliases

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -1,15 +1,37 @@
 # .aliases
 # vim:syntax=sh
 
-# git branches
+alias g="git"
+
+# branches
 alias develop_branch="git co develop"
 alias master_branch="git co master"
 alias stable_branch="git co stable"
 
-# git helpers
-alias gx="gitx"
+# rebase
 alias rebase_master="git pull --rebase origin master"
 alias rebase_stable="git pull --rebase origin stable"
+
+# tools
+alias gk="gitk"
+alias gx="gitx"
+
+# logs - using functions defined in githelpers.zsh
 alias gld="_git_log_format_deploy -20"
 alias glp="_git_log_format_pretty -20"
 
+alias ga='git add .'
+alias gam='git amend'
+alias gl='git pull --prune'
+alias gp='git push origin HEAD'
+alias gd='git diff'
+alias gdt='git difftool'
+alias gc='git commit'
+alias gca='git commit -am'
+alias gco='git checkout'
+alias gb='git branch'
+alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
+alias grm="git status | grep deleted | awk '{print \$3}' | xargs git rm"
+
+alias gsh='git stash'
+# sh = stash
