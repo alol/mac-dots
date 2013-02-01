1bc9f587075ca6fef8b62bb43b356661e8194f6d
Ed James <ed.james.email@gmail.com>
2013-02-01 10:46:51 +0000

    show banner after install

---
+++ zsh/banner.zsh
@@ -0,0 +1,11 @@
+# .banner
+# vim:syntax=sh
+
+function _print_macdots_banner {
+  echo
+  printf '\033[0;32m%s\033[0m\n' '                              |       |        '
+  printf '\033[0;32m%s\033[0m\n' ' __ `__ \   _` |  __|      _` |  _ \  __|  __| '
+  printf '\033[0;32m%s\033[0m\n' ' |   |   | (   | ( _____| (   | (   | |  \__ \ '
+  printf '\033[0;32m%s\033[0m\n' '_|  _|  _|\__,_|\___|    \__,_|\___/ \__|____/ '
+  echo
+}
