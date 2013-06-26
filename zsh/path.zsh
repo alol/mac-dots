0c19044e61f21668dc8ffaf8d08a2313fe59cc3a
Ed James <ed.james.email@gmail.com>
2013-06-26 12:21:15 +0100

    added AWS CLI config with automated setup; bump version

--- zsh/path.zsh
+++ zsh/path.zsh
@@ -5,3 +5,9 @@ export PATH=/usr/local/bin:/usr/local/sbin:$MACDOTS/bin:$PATH;
 
 ### Added by the Heroku Toolbelt
 export PATH="/usr/local/heroku/bin:$PATH"
+
+### Added for Python
+export PATH="/usr/local/share/python:$PATH"
+
+### AWS CLI config file
+export AWS_CONFIG_FILE="~/.awsrc"
