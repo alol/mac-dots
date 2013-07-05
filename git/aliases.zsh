19ea73cc1afabc5fa76297530f5b2f642321dfcc
Ed James <ed.james.email@gmail.com>
2013-07-05 15:25:41 +0100

    load aliases after shell initialize to allow our own aliases to take precedence

--- git/aliases.zsh
+++ git/aliases.zsh
@@ -20,6 +20,7 @@ alias gx="gitx"
 alias gld="_git_log_format_deploy -20"
 alias glp="_git_log_format_pretty -20"
 
+alias ga="git add ."
 alias gad="git add ."
 alias gam='git commit --amend'
 alias gl='git pull --prune'
@@ -28,8 +29,7 @@ alias gpf='git push -f'
 alias gd='git diff'
 alias gdt='git difftool'
 alias gc='git commit'
-alias gca="git commit -a"
-alias gcam="git commit -am"
+alias gca="git commit -am"
 alias gco='git checkout'
 alias gb='git branch'
 alias gs='git status -sb' # upgrade your git if -sb breaks for you. it's fun.
