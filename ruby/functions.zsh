88218d7c3399c94565da2760507d7476e126ca78
Ed James <ed.james.email@gmail.com>
2013-09-03 15:37:31 +0100

    added function for swapping RVM gemsets

--- ruby/functions.zsh
+++ ruby/functions.zsh
@@ -1,11 +1,24 @@
 # .functions
 # vim:syntax=sh
 
-#####################
+###################
 # Rails functions #
-#####################
+###################
 
 # Upgrade empty all log files
 function erl {
   for i in `l log/*.log`; do cat /dev/null > $i; done
 }
+
+# Change between 2 RVM gemsets
+function swap_gemset {
+	if [ -f .rvmrc ] && [ -f .rvmrc.swap ] ; then
+		mv .rvmrc .rvmrc.temp
+		mv .rvmrc.swap .rvmrc
+		mv .rvmrc.temp .rvmrc.swap
+		cd .. && cd -
+	else
+		echo "Uh-oh! You don't seem to have a '.rvmrc.swap' file."
+	fi
+}
+
